dac01632000ef5923bd915d2d2f06446ee3299f7
webkey <serhiy_i@ukr.net>
2018-05-02 16:24:23 +0300

    office_20180502_1620

--- src/js/common.js
+++ src/js/common.js
@@ -314,6 +314,9 @@ function fullPageInitial() {
 			navigationPosition: null
 			// navigationTooltips: ['First', 'Second', 'Third', '4', 'foo'],
 			// showActiveTooltip: true
+			, onLeave: function (index, nextIndex, direction) {
+				$('html').toggleClass('scroll-is-bottom', (direction === "down" && index === 4))
+			}
 		});
 	}
 
@@ -324,8 +327,176 @@ function fullPageInitial() {
 			$.fn.fullpage.moveSectionDown();
 		}
 	});
+
 }
 /*full page scroll*/
+
+/**
+ * !page bottoming
+ * */
+function bottoming() {
+	$('html').toggleClass('scroll-is-bottom', ($(window).scrollTop() === $(document).height() - $(window).height()))
+}
+
+$(window).on('load debouncedresize scroll', function () {
+	bottoming();
+});
+
+/**
+ * !Plugin HoverClass
+ * */
+(function ($) {
+	var HoverClass = function (settings) {
+		var options = $.extend({
+			container: 'ul',
+			item: 'li',
+			drop: 'ul'
+		},settings || {});
+
+		var self = this;
+		self.options = options;
+
+		var container = $(options.container);
+		self.$container = container;
+		self.$item = $(options.item, container);
+		self.$drop = $(options.drop, container);
+		self.$html = $('html');
+
+		self.modifiers = {
+			hover: 'hover',
+			hoverNext: 'hover_next',
+			hoverPrev: 'hover_prev'
+		};
+
+		self.addClassHover();
+
+		if (!DESKTOP) {
+			$(window).on('debouncedresize', function () {
+				self.removeClassHover();
+			});
+		}
+	};
+
+	HoverClass.prototype.addClassHover = function () {
+		var self = this,
+			_hover = this.modifiers.hover,
+			_hoverNext = this.modifiers.hoverNext,
+			_hoverPrev = this.modifiers.hoverPrev,
+			$item = self.$item,
+			item = self.options.item,
+			$container = self.$container;
+
+		if (!DESKTOP) {
+
+			$container.on('click', ''+item+'', function (e) {
+				var $currentAnchor = $(this);
+				var currentItem = $currentAnchor.closest($item);
+
+				if (!currentItem.has(self.$drop).length){ return; }
+
+				e.stopPropagation();
+
+				if (currentItem.hasClass(_hover)){
+					// self.$html.removeClass('css-scroll-fixed');
+
+					// if($('.main-sections-js').length) {
+					// 	$.fn.fullpage.setAllowScrolling(true); // unblocked fullpage scroll
+					// }
+
+					currentItem.removeClass(_hover).find('.'+_hover+'').removeClass(_hover);
+
+					return;
+				}
+
+				// self.$html.addClass('css-scroll-fixed');
+				// if($('.main-sections-js').length) {
+				// 	$.fn.fullpage.setAllowScrolling(false); // blocked fullpage scroll
+				// }
+
+				$('.'+_hover+'').not($currentAnchor.parents('.'+_hover+''))
+					.removeClass(_hover)
+					.find('.'+_hover+'')
+					.removeClass(_hover);
+				currentItem.addClass(_hover);
+
+				e.preventDefault();
+			});
+
+			$container.on('click', ''+self.options.drop+'', function (e) {
+				e.stopPropagation();
+			});
+
+			$(document).on('click', function () {
+				$item.removeClass(_hover);
+			});
+
+		} else {
+			$container.on('mouseenter', ''+item+'', function () {
+
+				var currentItem = $(this);
+
+				if (currentItem.prop('hoverTimeout')) {
+					currentItem.prop('hoverTimeout', clearTimeout(currentItem.prop('hoverTimeout')));
+				}
+
+				currentItem.prop('hoverIntent', setTimeout(function () {
+					console.log('mouseenter');
+					// self.$html.addClass('css-scroll-fixed');
+					// if($('.main-sections-js').length) {
+					// 	$.fn.fullpage.setAllowScrolling(false); // blocked fullpage scroll
+					// }
+
+					currentItem.addClass(_hover);
+					currentItem.next().addClass(_hoverNext);
+					currentItem.prev().addClass(_hoverPrev);
+
+				}, 50));
+
+			}).on('mouseleave', ''+ item+'', function () {
+
+				var currentItem = $(this);
+
+				if (currentItem.prop('hoverIntent')) {
+					currentItem.prop('hoverIntent', clearTimeout(currentItem.prop('hoverIntent')));
+				}
+
+				currentItem.prop('hoverTimeout', setTimeout(function () {
+					console.log('mouseleave');
+					// self.$html.removeClass('css-scroll-fixed');
+					// if($('.main-sections-js').length) {
+					// 	$.fn.fullpage.setAllowScrolling(true); // unblocked fullpage scroll
+					// }
+
+					currentItem.removeClass(_hover);
+					currentItem.next().removeClass(_hoverNext);
+					currentItem.prev().removeClass(_hoverPrev);
+				}, 50));
+
+			});
+
+		}
+	};
+
+	HoverClass.prototype.removeClassHover = function () {
+		var self = this;
+		self.$item.removeClass(self.modifiers.hover );
+	};
+
+	window.HoverClass = HoverClass;
+
+}(jQuery));
+
+/**
+ * !Toggle "hover" class by hover on the item of the list
+ * */
+function initHoverClass(){
+	if($('.menu-list').length){
+		new HoverClass({
+			container: '.menu-list',
+			drop: '.js-nav-drop'
+		});
+	}
+}
 
 /**
  * !Initial sliders on the project
@@ -1863,33 +2034,6 @@ function equalHeight() {
 /**
  * !toggle view shops
  * */
-/*function toggleView() {
-	var $switcherHand = $('.view-switcher-js a');
-
-	if ( $switcherHand.length ) {
-
-		var $container = $('.view-container-js');
-		var activeHand = 'active';
-		var activeContainer = 'grid-view';
-
-		$switcherHand.on('click', function (e) {
-			e.preventDefault();
-
-			var $this = $(this);
-
-			if ( $this.hasClass(activeHand) ) return false;
-
-			$switcherHand.removeClass(activeHand);
-			// $container.removeClass(activeContainer);
-
-			$this.addClass(activeHand);
-			$container.toggleClass(activeContainer);
-			// if ($this.index() === 0) {
-			// 	$container.addClass(activeContainer);
-			// }
-		});
-	}
-}*/
 
 (function($){
 	// <!--view switcher-->
@@ -2779,18 +2923,147 @@ function initMultiAccordion() {
 			animateSpeed: 200
 		});
 	}
+}
 
-	var navMenu = $('.nav-js');
+/**
+ * !multi accordion jquery plugin
+ * */
+(function ($) {
+	var MultiAccordion = function (settings) {
+		var options = $.extend({
+			collapsibleAll: false, // если установить значение true, сворачиваются идентичные панели НА СТРАНИЦЕ, кроме текущего
+			resizeCollapsible: false, // если установить значение true, при ресайзе будут соворачиваться все элементы
+			container: null, // общий контейнер
+			item: null, // непосредственный родитель открывающегося элемента
+			handler: null, // открывающий элемента
+			handlerWrap: null, // если открывающий элемент не является непосредственным соседом открывающегося элемента, нужно указать элемент, короный является оберткой открывающего элемета и лежит непосредственно перед открывающимся элементом (условно, является табом)
+			panel: null, // открывающийся элемент
+			openClass: 'active', // класс, который добавляется при открытии
+			currentClass: 'current', // класс текущего элемента
+			animateSpeed: 300, // скорость анимации
+			collapsible: false // сворачивать соседние панели
+		}, settings || {});
 
-	if (navMenu.length) {
-		new JsAccordion({
-			accordionContainer: '.nav-js',
-			accordionItem: 'li',
-			accordionHeader: '.nav__tab-js',
-			accordionHand: '.nav-handler-js',
-			indexInit: false,
-			clickOutside: false,
-			animateSpeed: 200
+		this.options = options;
+		var container = $(options.container);
+		this.$container = container;
+		this.$item = $(options.item, container);
+		this.$handler = $(options.handler, container);
+		this.$handlerWrap = $(options.handlerWrap, container);
+		this._animateSpeed = options.animateSpeed;
+		this.$totalCollapsible = $(options.totalCollapsible);
+		this._resizeCollapsible = options.resizeCollapsible;
+
+		this.modifiers = {
+			active: options.openClass,
+			current: options.currentClass
+		};
+
+		this.bindEvents();
+		this.totalCollapsible();
+		this.totalCollapsibleOnResize();
+
+	};
+
+	MultiAccordion.prototype.totalCollapsible = function () {
+		var self = this;
+		self.$totalCollapsible.on('click', function () {
+			self.$panel.slideUp(self._animateSpeed, function () {
+				self.$container.trigger('accordionChange');
+			});
+			self.$item.removeClass(self.modifiers.active);
+		})
+	};
+
+	MultiAccordion.prototype.totalCollapsibleOnResize = function () {
+		var self = this;
+		$(window).on('resize', function () {
+			if (self._resizeCollapsible) {
+				self.$panel.slideUp(self._animateSpeed, function () {
+					self.$container.trigger('accordionChange');
+				});
+				self.$item.removeClass(self.modifiers.active);
+			}
+		});
+	};
+
+	MultiAccordion.prototype.bindEvents = function () {
+		var self = this;
+		var $container = this.$container;
+		var $item = this.$item;
+		var panel = this.options.panel;
+
+		$container.on('click', self.options.handler, function (e) {
+			var $currentHandler = self.options.handlerWrap ? $(this).closest(self.options.handlerWrap) : $(this);
+			var $currentItem = $currentHandler.closest($item);
+
+			if ($currentItem.has($(panel)).length) {
+				e.preventDefault();
+
+				if ($currentHandler.next(panel).is(':visible')) {
+					self.closePanel($currentItem);
+
+					return;
+				}
+
+				if (self.options.collapsibleAll) {
+					self.closePanel($($container).not($currentHandler.closest($container)).find($item));
+				}
+
+				if (self.options.collapsible) {
+					self.closePanel($currentItem.siblings());
+				}
+
+				self.openPanel($currentItem, $currentHandler);
+			}
+		})
+	};
+
+	MultiAccordion.prototype.closePanel = function ($currentItem) {
+		var self = this;
+		var panel = self.options.panel;
+		var openClass = self.modifiers.active;
+
+		$currentItem.removeClass(openClass).find(panel).filter(':visible').slideUp(self._animateSpeed, function () {
+			self.$container.trigger('mAccordionAfterClose').trigger('mAccordionAfterChange');
+		});
+
+		$currentItem
+			.find(self.$item)
+			.removeClass(openClass);
+	};
+
+	MultiAccordion.prototype.openPanel = function ($currentItem, $currentHandler) {
+		var self = this;
+		var panel = self.options.panel;
+
+		$currentItem.addClass(self.modifiers.active);
+
+		$currentHandler.next(panel).slideDown(self._animateSpeed, function () {
+			self.$container.trigger('mAccordionAfterOpened').trigger('mAccordionAfterChange');
+		});
+	};
+
+	window.MultiAccordion = MultiAccordion;
+}(jQuery));
+
+/**
+ * !multi accordion initial
+ * */
+function ininitNavAccordion() {
+
+	var navMenu = '.nav-js';
+
+	if ($(navMenu).length) {
+		new MultiAccordion({
+			container: navMenu,
+			item: 'li',
+			handler: '.nav-handler-js',
+			handlerWrap: '.nav__tab-js',
+			panel: '.nav-drop-js',
+			openClass: 'is-open',
+			animateSpeed: 200,
+			collapsible: true
 		});
 	}
 }
@@ -3399,6 +3672,7 @@ $(document).ready(function () {
 	customSelect($('select.cselect'));
 	fileInput();
 	fullPageInitial();
+	initHoverClass();
 	slidersInit();
 	objectFitImages(); // object-fit-images initial
 	shuttersInit();
@@ -3412,6 +3686,7 @@ $(document).ready(function () {
 	multiFiltersInit();
 	sortingOrder();
 	initMultiAccordion();
+	ininitNavAccordion();
 	popupInitial();
 	spinnerInit($(".spinner-js"));
 	// tooltipInit();
